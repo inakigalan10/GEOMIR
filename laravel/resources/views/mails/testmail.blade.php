61fa1daaa09f7d7123e166a6d5807286f7d1e4f7
Iñaki Galan Mata <inakigalan17@gmail.com>
2022-10-14 17:33:30 +0200

    3.2.2

--- laravel/resources/views/mails/testmail.blade.php
+++ laravel/resources/views/mails/testmail.blade.php
@@ -1,12 +1,12 @@
-<x-mail::message>
-# Introduction
-
-The body of your message.
-
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
-
+@component('mail::message')
+# Hello {{$content['name']}},
+ 
+{{$content['body']}}
+ 
+@component('mail::button', ['url' => $content['url']])
+Click Here
+@endcomponent
+ 
 Thanks,<br>
 {{ config('app.name') }}
-</x-mail::message>
+@endcomponent
