f3c893c72a968852a4639fcfe583aabe26fafccb
Iñaki Galan Mata <inakigalan17@gmail.com>
2022-10-17 19:11:33 +0200

    4.2

--- laravel/routes/auth.php
+++ laravel/routes/auth.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
+
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -53,4 +55,8 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+
+
+    Route::get('mail/test', [MailController::class, 'test']);
+
 });
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -24,5 +24,4 @@ Route::get('/dashboard', function () {
 
 // ...
 
-
 require __DIR__.'/auth.php';
