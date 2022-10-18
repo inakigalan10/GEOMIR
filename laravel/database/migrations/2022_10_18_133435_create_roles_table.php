70685295f456b22dfa4f728b3116b9eeca1a5390
Iñaki Galan Mata <inakigalan17@gmail.com>
2022-10-18 17:00:20 +0200

    5.1

---
+++ laravel/database/migrations/2022_10_18_133435_create_roles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoincrement();
+            $table->primary('id');
+            $table->string('name')->unique();
+        });
+        Schema::table ('users', function (Blueprint $table){
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table ('users', function (Blueprint $table){
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
+        });
+        Schema::drop('roles');
+    }
+};
