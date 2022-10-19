183540c32c1cf621e6413926a4fc899ab8d4c6a3
Iñaki Galan Mata <inakigalan17@gmail.com>
2022-10-19 18:55:50 +0200

    7.2

--- laravel/resources/views/layouts/guest.blade.php
+++ laravel/resources/views/layouts/guest.blade.php
@@ -1,3 +1,5 @@
+@include('flash')
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/', function (Request $request) {
+    $message = 'Loading welcome page';
+    Log::info($message);
+    $request->session()->flash('info', $message);
+    return view('welcome');
+ });
+ 
 // ...
 
 require __DIR__.'/auth.php';
