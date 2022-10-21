35715b23fa31c05b89d0a086bfe1a95c356204ed
Iñaki Galan Mata <inakigalan17@gmail.com>
2022-10-21 18:53:37 +0200

    8.2.2

--- laravel/app/Http/Controllers/FileController.php
+++ laravel/app/Http/Controllers/FileController.php
@@ -27,7 +27,7 @@ class FileController extends Controller
      */
     public function create()
     {
-        //
+        return view("files.create");
     }
 
     /**
@@ -38,8 +38,47 @@ class FileController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validar fitxer
+        $validatedData = $request->validate([
+            'upload' => 'required|mimes:gif,jpeg,jpg,png|max:1024'
+        ]);
+       
+        // Obtenir dades del fitxer
+        $upload = $request->file('upload');
+        $fileName = $upload->getClientOriginalName();
+        $fileSize = $upload->getSize();
+        \Log::debug("Storing file '{$fileName}' ($fileSize)...");
+  
+        // Pujar fitxer al disc dur
+        $uploadName = time() . '_' . $fileName;
+        $filePath = $upload->storeAs(
+            'uploads',      // Path
+            $uploadName ,   // Filename
+            'public'        // Disk
+        );
+       
+        if (\Storage::disk('public')->exists($filePath)) {
+            \Log::debug("Local storage OK");
+            $fullPath = \Storage::disk('public')->path($filePath);
+            \Log::debug("File saved at {$fullPath}");
+            // Desar dades a BD
+            $file = File::create([
+                'filepath' => $filePath,
+                'filesize' => $fileSize,
+            ]);
+            \Log::debug("DB storage OK");
+            // Patró PRG amb missatge d'èxit
+            return redirect()->route('files.show', $file)
+                ->with('success', 'File successfully saved');
+        } else {
+            \Log::debug("Local storage FAILS");
+            // Patró PRG amb missatge d'error
+            return redirect()->route("files.create")
+                ->with('error', 'ERROR uploading file');
+        }
     }
+ 
+ 
 
     /**
      * Display the specified resource.
