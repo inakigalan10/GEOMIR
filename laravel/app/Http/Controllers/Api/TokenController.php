e79d1ba49347b010e8c5fb1836d450c392daa3d9
Iñaki Galan Mata <inakigalan17@gmail.com>
2022-12-20 16:42:23 +0100

    si

--- laravel/app/Http/Controllers/Api/TokenController.php
+++ laravel/app/Http/Controllers/Api/TokenController.php
@@ -46,6 +46,45 @@ class TokenController extends Controller
        }
    }
 
+   public function register(Request $request)
+   {
+       $request->validate([
+           'name' => ['required', 'string', 'max:255'],
+           'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+           'password' => ['required', 'confirmed', Rules\Password::defaults()],
+
+       ]);
+
+       $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => Hash::make($request->password),
+       ]);
+       if (Auth::attempt($credentials)) {
+        // Get user
+        $user = User::where([
+            ["email", "=", $credentials["email"]]
+        ])->firstOrFail();
+        // Revoke all old tokens
+        $user->tokens()->delete();
+        // Generate new token
+        $token = $user->createToken("authToken")->plainTextToken;
+        // Token response
+        return response()->json([
+            "success"   => true,
+            "authToken" => $token,
+            "tokenType" => "Bearer"
+        ], 200);
+        } else {
+        return response()->json([
+            "success" => false,
+            "message" => "Invalid login credentials"
+        ], 401);
+
+    
+   }
 
 
+
+}
 }
--- laravel/vendor/symfony/http-kernel/DataCollector/RequestDataCollector.php
+++ laravel/vendor/symfony/http-kernel/DataCollector/RequestDataCollector.php
@@ -36,7 +36,7 @@ class RequestDataCollector extends DataCollector implements EventSubscriberInter
      */
     private \SplObjectStorage $controllers;
     private array $sessionUsages = [];
-    private ?RequestStack $requestStack;
+    private $requestStack;
 
     public function __construct(RequestStack $requestStack = null)
     {
@@ -44,6 +44,9 @@ class RequestDataCollector extends DataCollector implements EventSubscriberInter
         $this->requestStack = $requestStack;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function collect(Request $request, Response $response, \Throwable $exception = null)
     {
         // attributes are serialized and as they can be anything, they need to be converted to strings.
@@ -388,6 +391,9 @@ class RequestDataCollector extends DataCollector implements EventSubscriberInter
         ];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName(): string
     {
         return 'request';
@@ -443,7 +449,7 @@ class RequestDataCollector extends DataCollector implements EventSubscriberInter
                     'file' => $r->getFileName(),
                     'line' => $r->getStartLine(),
                 ];
-            } catch (\ReflectionException) {
+            } catch (\ReflectionException $e) {
                 if (\is_callable($controller)) {
                     // using __call or  __callStatic
                     return [
@@ -471,7 +477,7 @@ class RequestDataCollector extends DataCollector implements EventSubscriberInter
             }
             $controller['method'] = $r->name;
 
-            if ($class = \PHP_VERSION_ID >= 80111 ? $r->getClosureCalledClass() : $r->getClosureScopeClass()) {
+            if ($class = $r->getClosureScopeClass()) {
                 $controller['class'] = $class->name;
             } else {
                 return $r->name;
