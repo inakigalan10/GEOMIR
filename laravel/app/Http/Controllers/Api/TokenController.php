1f0f86bb5db1916d9cd476452b68961763f001bf
inakigalan10 <inakigalan17@gmail.com>
2023-01-04 22:35:19 +0100

    api de token

--- laravel/app/Http/Controllers/Api/TokenController.php
+++ laravel/app/Http/Controllers/Api/TokenController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class TokenController extends Controller
 {
     public function user(Request $request)
-   {
+    {
        $user = User::where('email', $request->user()->email)->first();
       
        return response()->json([
@@ -16,75 +19,70 @@ class TokenController extends Controller
            "user"    => $request->user(),
            "roles"   => $user->getRoleNames(),
        ]);
-   }
-   public function login(Request $request)
-   {
-       $credentials = $request->validate([
-           'email'     => 'required|email',
-           'password'  => 'required',
-       ]);
-       if (Auth::attempt($credentials)) {
-           // Get user
-           $user = User::where([
-               ["email", "=", $credentials["email"]]
-           ])->firstOrFail();
-           // Revoke all old tokens
-           $user->tokens()->delete();
-           // Generate new token
-           $token = $user->createToken("authToken")->plainTextToken;
-           // Token response
-           return response()->json([
-               "success"   => true,
-               "authToken" => $token,
-               "tokenType" => "Bearer"
-           ], 200);
-       } else {
-           return response()->json([
-               "success" => false,
-               "message" => "Invalid login credentials"
-           ], 401);
-       }
-   }
-
-   public function register(Request $request)
-   {
-       $request->validate([
-           'name' => ['required', 'string', 'max:255'],
-           'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-           'password' => ['required', 'confirmed', Rules\Password::defaults()],
-
-       ]);
-
-       $user = User::create([
-           'name' => $request->name,
-           'email' => $request->email,
-           'password' => Hash::make($request->password),
-       ]);
-       if (Auth::attempt($credentials)) {
-        // Get user
-        $user = User::where([
-            ["email", "=", $credentials["email"]]
-        ])->firstOrFail();
-        // Revoke all old tokens
-        $user->tokens()->delete();
-        // Generate new token
-        $token = $user->createToken("authToken")->plainTextToken;
-        // Token response
-        return response()->json([
-            "success"   => true,
-            "authToken" => $token,
-            "tokenType" => "Bearer"
-        ], 200);
-        } else {
-        return response()->json([
-            "success" => false,
-            "message" => "Invalid login credentials"
-        ], 401);
-
+    }
     
-   }
+    public function register(Request $request)
+    {
+        
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
+        if ($data) {
+            $user = User::create([
+                'name'     => $data['name'],
+                'email'    => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+            $user->assignRole('author');
 
+            $token = $user->createToken('authToken')->plainTextToken;
+            
+            return response()->json([
+                'success' => true,
+                'authToken' => $token,
+                'tokenType' => 'Bearer',
+            ], 200);
+        }
+    }
 
-}
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'     => 'required|email',
+            'password'  => 'required',
+        ]);
+        if (Auth::attempt($credentials)) {
+            // Get user
+            $user = User::where([
+                ["email", "=", $credentials["email"]]
+            ])->firstOrFail();
+            // Revoke all old tokens
+            $user->tokens()->delete();
+            // Generate new token
+            $token = $user->createToken("authToken")->plainTextToken;
+            // Token response
+            return response()->json([
+                "success"   => true,
+                "authToken" => $token,
+                "tokenType" => "Bearer"
+            ], 200);
+        }   else {
+            return response()->json([
+                "success" => false,
+                "message" => "Invalid login credentials"
+            ], 401);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        return response()->json([
+            'success' => true,
+            'message'    => "See you soon!",
+        ], 200);    
+    }    
+    
 }
